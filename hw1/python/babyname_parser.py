623f5275e3564e43b968917842b9d9cd5fe5290a
BAIK SEUNGWOOK <baiksw1997@gmail.com>
2018-09-05 22:30:23 +0900

    9_05_modify

--- hw1/python/babyname_parser.py
+++ hw1/python/babyname_parser.py
@@ -11,17 +11,15 @@
 
 import sys
 import re
+import os.path
 
 """Baby Names exercise
-
 Implement the babyname parser class that parses the popular names and their ranks from an html file.
-
 1) At first, you need to implement a decorator that checks whether the html file exists or not.
 2) Also, the parser should extract the tuples of (rank, male-name, female-name) from the file by using regex. 
    For writing regex, it's nice to include a copy of the target text for inspiration.
 3) Finally, you need to implement the parse(self, parsing_lambda) method that parses the extracted tuples
    with the given lambda and return the list of processed results.
-
 Here's what the html looks like in the baby.html files:
     ...
     <h3 align="center">Popularity in 1990</h3>
@@ -43,13 +41,22 @@ class BabynameFileNotFoundException(Exception):
 def check_filename_existence(func):
     """
     A decorator that catches the non-exiting filename argument and raises a custom `BabynameFileNotFoundException`.
-
     Args:
         func: The function to decorate.
     Raises:
         BabynameFileNotFoundException: if there is no such file named as the first argument of the function to decorate.
     """
     # TODO: Implement this decorator.
+    def inner(func,filename):
+        try:
+            if not os.path.isfile(filename):
+                raise BabynameFileNotFoundException()
+            return func(filename)
+        except BabynameFileNotFoundException:
+            print("No such babyname file or directory: "+filename)
+            sys.exit(1)
+
+    return inner
 
 
 class BabynameParser:
@@ -60,13 +67,14 @@ class BabynameParser:
         Given a file name for baby.html, extracts the year of the file and
         a list of the (rank, male-name, female-name) tuples from the file by using regex.
         [('1', 'Michael', 'Jessica'), ('2', 'Christopher', 'Ashley'), ....]
-
         Args:
             filename: The filename to parse.
         """
 
         text = "File is not read yet"  # TODO: Open and read the given file.
         # Could process the file line-by-line, but regex on the whole text at once is even easier.
+        file = open(filename,'r')
+        text = file.readline()
 
         # The year extracting code is provided. Implement the tuple extracting code by using this.
         year_match = re.search(r'Popularity\sin\s(\d{4})', text)
@@ -79,12 +87,17 @@ class BabynameParser:
         # Extract all the data tuples with a findall()
         # each tuple is: (rank, male-name, female-name)
         self.rank_to_names_tuples = []  # TODO: Extract the list of rank to names tuples.
+        entries = re.split("\n+",text)
+        for entry in entries:
+            other_match = re.match(r"<td>(\d+)</td><td>(\w+)</td><td>(\w+)</td>",entry)
+            if other_match:
+                self.rank_to_names_tuples[-1] = (other_match.group(1),other_match.group(2),other_match.group(3))
+
 
     def parse(self, parsing_lambda):
         """
         Collects a list of babynames parsed from the (rank, male-name, female-name) tuples.
         The list must contains all results processed with the given lambda.
-
         Args:
             parsing_lambda: The parsing lambda.
                             It must process an single (string, string, string) tuple and return something.
@@ -92,3 +105,8 @@ class BabynameParser:
             The list of parsed babynames.
         """
         # TODO: Implement this method.
+        return_list = []
+        for entry in self.rank_to_names_tuples:
+            return_list[-1] = parsing_lambda(entry)
+        return return_list
+
--- hw1/python/run.py
+++ hw1/python/run.py
@@ -15,7 +15,6 @@ from babyname_parser import BabynameParser
 """
 Parse an html file that contains the popular baby names in a year,
 sort them and find the common popular names with their rank.
-
 Here's what the expected results looks like:
     Common popular babynames in 1994 (Count: 85)
     Common babyname: male rank, female rank
@@ -31,6 +30,7 @@ def main():
     # This command-line parsing code is provided.
     # Make a list of command line arguments, omitting the [0] element
     # which is the script itself.
+
     args = sys.argv[1:]
 
     if len(args) < 1:
@@ -41,14 +41,31 @@ def main():
     parser = BabynameParser(filename)
 
     # Parse male-names and female-names from the file with the implemented parser.
-    parsed_malenames = parser.parse(None)  # TODO: Parse the rank and male-name tuples with your lambda.
-    parsed_femalenames = parser.parse(None)  # TODO: Parse the rank and female-name tuples with your lambda.
+    parsed_malenames = parser.parse(lambda rank_tuple : rank_tuple[1])  # TODO: Parse the rank and male-name tuples with your lambda.
+    parsed_femalenames = parser.parse(lambda rank_tuple : rank_tuple[2])  # TODO: Parse the rank and female-name tuples with your lambda.
 
     # Find the common popular names.
     common_names = []
     # TODO: Fill the common_names with (common popular babyname: male-rank, female-rank) strings.
     # TODO: Sort the list in ascending alphabetical order.
-
+    malenames_sorted = sorted(parser.rank_to_names_tuples, key = lambda rank_tuple : rank_tuple[1])
+    femalenames_sorted = sorted(parser.rank_to_names_tuples, key = lambda rank_tuple : rank_tuple[2])
+    parsed_malenames.sort()
+    parsed_femalenames.sort()
+    malename_idx = 0
+    femalename_idx = 0
+    while True:
+        if malename_idx is len(parsed_malenames) or femalename_idx is len(parsed_femalenames):
+            break
+        if parsed_malenames[malename_idx] > parsed_femalenames[femalename_idx]:
+            femalename_idx += 1
+        elif parsed_malenames[malename_idx] < parsed_femalenames[femalename_idx]:
+            malename_idx += 1
+        else:
+            common_names[-1] = parsed_malenames[malename_idx] + ": " + malenames_sorted[malename_idx][0] + ", " + femalenames_sorted[femalename_idx][0]
+            femalename_idx += 1
+            malename_idx += 1
+            
     # Print your result.
     print("Common popular babynames in {0} (Count: {1})".format(parser.year, str(len(common_names))))
     print("Common babyname: male rank, female rank")
